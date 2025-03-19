66bc53f5fc6dbde782b47ef552ed0fdaa65782b2
Johan Polo <johan.s310806@gmail.com>
2025-03-18 20:26:16 -0500

    Aplicación de try catch en cada uno de los metodos que se ejecuta cuando se ingresa el número del ejercicio

--- app.js
+++ app.js
@@ -12,52 +12,64 @@ import { getCommets } from "./Modulos/Comments/index.js";
 
 // Declaro variable a la cual le asigno una arrow funtion la cual retornará una promesa ya que se convierte en asincrona. El resultado de esta promesa es donde se almacena el resultado del ejercicio 1. Es decir, las tareas pendientes por cada usuario.
 const getTareasPendings = async () => {
-    // Declaro variable usuarios donde se almacenará los usuarios traidos de la peticion.
-    const usuarios =  await getUsuarios(URL);
-    // Se usa promise all para ejecutar multiples promesas (una por cada usuario). Luego, Recorro el arreglo de usuarios con "map", creando una promesa asíncrona para cada usuario y lo que retorne la funcion map es lo que retornará la función get usuarios si se cumple la promesa.
-    return await Promise.all(usuarios.map(async (usuario) => {
-      // Declaro varaible tareasPending que almacenará el arreglo donde se almacenan las tareas pendiente por cada usuario.
-      const pendindTasks = await getTodo(URL, usuario);
-      // Retorno un nuevo objeto que contiene los datos del usuario (...usuario) con sus tareas pendientes(tareasPending).
-      return {...usuario, pendindTasks}
-    }));
+    try { //Uso try para recorrer el bloque de la funcion. Si en algun momento se llega a presentar un error automaticamente se manda al catch.
+        // Declaro variable usuarios donde se almacenará los usuarios traidos de la peticion.
+        const usuarios =  await getUsuarios(URL);
+        // Se usa promise all para ejecutar multiples promesas (una por cada usuario). Luego, Recorro el arreglo de usuarios con "map", creando una promesa asíncrona para cada usuario y lo que retorne la funcion map es lo que retornará la función get usuarios si se cumple la promesa.
+        return await Promise.all(usuarios.map(async (usuario) => {
+        // Declaro varaible tareasPending que almacenará el arreglo donde se almacenan las tareas pendiente por cada usuario.
+        const pendindTasks = await getTodo(URL, usuario);
+        // Retorno un nuevo objeto que contiene los datos del usuario (...usuario) con sus tareas pendientes(tareasPending).
+        return {...usuario, pendindTasks}
+        }));
+    } catch (error) { //En está sección se recibe el error que se generó en el catch y se muestra por consola.
+        console.error(error);
+    }
 }
 
 
 // Declaro variable a la cual le asigno una arrow funtion que recibe como parametro un username la cual retornará una promesa ya que se convierte en asincrona. El resultado de esta promesa es donde se almacena el resultado del ejercicio 2. Es decir, los usuarios con sus albunes y fotos dependiendo del username ingresado.
 const getUserPorUserName = async (username) => {
-    // Declaro variable usuarios donde se almacenará los usuarios traidos de la peticion, en está petición se pasa como argumento la URL y el username recibido como parametro.
-    const usuario = await getUsuariosUserName(URL, username);
-    // Se usa promise all para ejecutar multiples promesas (una por cada usuario). Luego, Recorro el arreglo de usuarios con "map", creando una promesa asíncrona para cada usuario y lo que retorne la funcion map es lo que retornará la función getUserPorUserName si se cumple la promesa.
-    return await Promise.all(usuario.map(async (usuario) => {
-      // Declaro la variable "albums" donde se almacenará el arreglo con los albunes de cada usuario recibido de la petición.
-      const albums = await getAlbums(URL, usuario);
-      // Declaro la variable "albumPhoto" donde se almacenará un arreglo con los albunes, pero cada album tendrá también sus photos
-      //  dentro. Para esto, uso "Promise.all" para recorrer cada album y obtener las photos asociados.
-      const albumPhoto = await Promise.all(albums.map(async (album) => {
-        // Declaro la variable "photos" donde se almacenará el arreglo con las photos del album actual recibido de la petición.
-        const photos = await getPhotos(URL, album);
-        // Retorno un nuevo objeto que contiene todas las propiedades de cada album y le agrego la propiedad "photos".
-        return { ...album, photos };
-      }));
-      // Retorno un nuevo objeto que contiene los datos del usuario (...usuario) y los albums con sus fotos (albumPhoto).
-      return {...usuario, albumPhoto};
-    }));
+    try { //Uso try para recorrer el bloque de la funcion. Si en algun momento se llega a presentar un error automaticamente se manda al catch.
+        // Declaro variable usuarios donde se almacenará los usuarios traidos de la peticion, en está petición se pasa como argumento la URL y el username recibido como parametro.
+        const usuario = await getUsuariosUserName(URL, username);
+        // Se usa promise all para ejecutar multiples promesas (una por cada usuario). Luego, Recorro el arreglo de usuarios con "map", creando una promesa asíncrona para cada usuario y lo que retorne la funcion map es lo que retornará la función getUserPorUserName si se cumple la promesa.
+        return await Promise.all(usuario.map(async (usuario) => {
+        // Declaro la variable "albums" donde se almacenará el arreglo con los albunes de cada usuario recibido de la petición.
+        const albums = await getAlbums(URL, usuario);
+        // Declaro la variable "albumPhoto" donde se almacenará un arreglo con los albunes, pero cada album tendrá también sus photos
+        //  dentro. Para esto, uso "Promise.all" para recorrer cada album y obtener las photos asociados.
+        const albumPhoto = await Promise.all(albums.map(async (album) => {
+            // Declaro la variable "photos" donde se almacenará el arreglo con las photos del album actual recibido de la petición.
+            const photos = await getPhotos(URL, album);
+            // Retorno un nuevo objeto que contiene todas las propiedades de cada album y le agrego la propiedad "photos".
+            return { ...album, photos };
+        }));
+        // Retorno un nuevo objeto que contiene los datos del usuario (...usuario) y los albums con sus fotos (albumPhoto).
+        return {...usuario, albumPhoto};
+        }));
+    } catch (error) { //En está sección se recibe el error que se generó en el catch y se muestra por consola.
+        console.error(error);
+    }
 }
 
 
 
 // Declaro variable a la cual le asigno una arrow funtion la cual retornará una promesa ya que se convierte en asincrona. El resultado de esta promesa es donde se almacena el resultado del ejercicio 3. Es decir, los posts con sus comentarios dependiendo del nombre del post ingresado.
 const getPostPorTitulo = async (titulo) => {
-    // Declaro varaible "posts" donde se almacena el resultado obtenido al llamar al metodo donde se obtienen los post por el titulo ingresado, enviando como argumentos la URL y el titulo.
-    const posts = await getPostTitulo(URL, titulo)
-    // Se usa promise all para ejecutar multiples promesas (una por cada post). Luego, Recorro el arreglo de posts con "map", creando una promesa asíncrona para cada post.
-    return await Promise.all(posts.map(async(post)=>{
-      // Declaro la variable "comments" donde se almacenará el arreglo con los comentarios del post actual recibido de la petición.
-      const comments = await getCommets(URL,post);
-      // Retorno un nuevo objeto que contiene todas las propiedades de cada post y le agrego la propiedad "comments".
-      return {...post, comments};
-    }));
+    try { //Uso try para recorrer el bloque de la funcion. Si en algun momento se llega a presentar un error automaticamente se manda al catch.
+        // Declaro varaible "posts" donde se almacena el resultado obtenido al llamar al metodo donde se obtienen los post por el titulo ingresado, enviando como argumentos la URL y el titulo.
+        const posts = await getPostTitulo(URL, titulo)
+        // Se usa promise all para ejecutar multiples promesas (una por cada post). Luego, Recorro el arreglo de posts con "map", creando una promesa asíncrona para cada post.
+        return await Promise.all(posts.map(async(post)=>{
+        // Declaro la variable "comments" donde se almacenará el arreglo con los comentarios del post actual recibido de la petición.
+        const comments = await getCommets(URL,post);
+        // Retorno un nuevo objeto que contiene todas las propiedades de cada post y le agrego la propiedad "comments".
+        return {...post, comments};
+        }));
+    } catch (error) { //En está sección se recibe el error que se generó en el catch y se muestra por consola.
+        console.error(error);
+    }
 }
 
 
@@ -84,33 +96,37 @@ const getUsersNamePhone = async () => {
 
 // Declaro variable a la cual le asigno una arrow funtion la cual retornará una promesa ya que se convierte en asincrona. El resultado de esta promesa es donde se almacena el resultado del ejercicio 5. Es decir, los usuarios con sus posts y respectivos comentarios y con sus albums y sus repectivas fotos.
 const getAllData = async () => {
-    // Declaro variable usuarios donde se almacenará los usuarios traidos de la peticion.
-    const usuarios =  await getUsuarios(URL);
-    // Se usa promise all para ejecutar multiples promesas (una por cada usuario). Luego, Recorro el arreglo de usuarios con "map", creando una promesa asíncrona para cada usuario.
-    return await Promise.all(usuarios.map(async(usuario)=>{
-        // Declaro la variable "posts" donde se almacenará el arreglo con los posts de cada usuario recibido de la petición.
-        const posts = await getPostUserId(URL,usuario);
-        // Declaro la variable "comentPost" donde se almacenará un arreglo con los posts, pero cada post tendrá también sus comentarios
-        //  dentro. Para esto, uso "Promise.all" para recorrer cada post y obtener los comentarios asociados.
-        const comentPost = await Promise.all( posts.map(async(post)=>{
-            // Declaro la variable "coments" donde se almacenará el arreglo con los comentarios del post actual recibido de la petición.
-            const coments = await getCommets(URL,post);
-            // Retorno un nuevo objeto que contiene todas las propiedades de cada post y le agrego la propiedad "coments".
-            return {...post,coments};
+    try { //Uso try para recorrer el bloque de la funcion. Si en algun momento se llega a presentar un error automaticamente se manda al catch.
+        // Declaro variable usuarios donde se almacenará los usuarios traidos de la peticion.
+        const usuarios =  await getUsuarios(URL);
+        // Se usa promise all para ejecutar multiples promesas (una por cada usuario). Luego, Recorro el arreglo de usuarios con "map", creando una promesa asíncrona para cada usuario.
+        return await Promise.all(usuarios.map(async(usuario)=>{
+            // Declaro la variable "posts" donde se almacenará el arreglo con los posts de cada usuario recibido de la petición.
+            const posts = await getPostUserId(URL,usuario);
+            // Declaro la variable "comentPost" donde se almacenará un arreglo con los posts, pero cada post tendrá también sus comentarios
+            //  dentro. Para esto, uso "Promise.all" para recorrer cada post y obtener los comentarios asociados.
+            const comentPost = await Promise.all( posts.map(async(post)=>{
+                // Declaro la variable "coments" donde se almacenará el arreglo con los comentarios del post actual recibido de la petición.
+                const coments = await getCommets(URL,post);
+                // Retorno un nuevo objeto que contiene todas las propiedades de cada post y le agrego la propiedad "coments".
+                return {...post,coments};
+            }));
+            // Declaro la variable "albums" donde se almacenará el arreglo con los albunes de cada usuario recibido de la petición.
+            const albums = await getAlbums(URL,usuario);
+            // Declaro la variable "albumPhoto" donde se almacenará un arreglo con los albunes, pero cada album tendrá también sus photos
+            //  dentro. Para esto, uso "Promise.all" para recorrer cada album y obtener las photos asociados.
+            const albumPhoto = await Promise.all( albums.map(async(album)=>{
+                // Declaro la variable "photos" donde se almacenará el arreglo con las photos del album actual recibido de la petición.
+                const photos = await getPhotos(URL,album);
+                // Retorno un nuevo objeto que contiene todas las propiedades de cada album y le agrego la propiedad "photos".
+                return {...album,photos};
+            }));
+            // Retorno un nuevo objeto que contiene los datos del usuario (...usuario), los post con sus comentarios (comentPost) y los albunes con sus fotos (albumPhoto).
+            return {...usuario,comentPost, albumPhoto};
         }));
-        // Declaro la variable "albums" donde se almacenará el arreglo con los albunes de cada usuario recibido de la petición.
-        const albums = await getAlbums(URL,usuario);
-        // Declaro la variable "albumPhoto" donde se almacenará un arreglo con los albunes, pero cada album tendrá también sus photos
-        //  dentro. Para esto, uso "Promise.all" para recorrer cada album y obtener las photos asociados.
-        const albumPhoto = await Promise.all( albums.map(async(album)=>{
-            // Declaro la variable "photos" donde se almacenará el arreglo con las photos del album actual recibido de la petición.
-            const photos = await getPhotos(URL,album);
-            // Retorno un nuevo objeto que contiene todas las propiedades de cada album y le agrego la propiedad "photos".
-            return {...album,photos};
-        }));
-        // Retorno un nuevo objeto que contiene los datos del usuario (...usuario), los post con sus comentarios (comentPost) y los albunes con sus fotos (albumPhoto).
-        return {...usuario,comentPost, albumPhoto};
-    }));
+    } catch (error) { //En está sección se recibe el error que se generó en el catch y se muestra por consola.
+        console.error(error);
+    }
 };
 
 
